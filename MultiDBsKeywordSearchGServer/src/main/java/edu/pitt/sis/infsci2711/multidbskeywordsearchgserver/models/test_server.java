2e33e52b55ace03d4e3cc1691a4a6df603199e18
fujunswufe <fujunswufe@gmail.com>
2015-03-26 13:46:50 -0400

    test
    test

--- MultiDBsKeywordSearchGServer/src/main/java/edu/pitt/sis/infsci2711/multidbskeywordsearchgserver/models/test_server.java
+++ MultiDBsKeywordSearchGServer/src/main/java/edu/pitt/sis/infsci2711/multidbskeywordsearchgserver/models/test_server.java
@@ -1,4 +1,4 @@
-package edu.pitt.sis.infsci2711.multidbskeywordsearchgserver.models;
+package edu.pitt.sis.infsci2711.multidbskeywordsearchgserver;
 
 public clas test_server {
 	public static void main(String[] args) {
--- MultiDBsKeywordSearchGServer/src/main/java/edu/pitt/sis/infsci2711/multidbskeywordsearchgserver/test_server.java
+++
@@ -1,7 +0,0 @@
-package edu.pitt.sis.infsci2711.multidbskeywordsearchgserver;
-
-public clas test_server {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
