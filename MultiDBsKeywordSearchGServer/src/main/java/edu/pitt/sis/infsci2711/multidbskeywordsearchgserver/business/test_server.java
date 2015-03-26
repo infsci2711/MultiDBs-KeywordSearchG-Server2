4b5897926775843a7f01f3ac4b8efcda5db31bf9
fujunswufe <fujunswufe@gmail.com>
2015-03-26 13:20:39 -0400

    test
    test

---
+++ MultiDBsKeywordSearchGServer/src/main/java/edu/pitt/sis/infsci2711/multidbskeywordsearchgserver/business/test_server.java
@@ -0,0 +1,7 @@
+package edu.pitt.sis.infsci2711.multidbskeywordsearchgserver.business;
+
+public clas test_server {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
