f3fd3527ff08a49b5d3443e286334908fd9016d6
keranzhao <kez17@pitt.edu>
2015-04-13 00:06:43 -0400

    AWS3

--- MultiDBsKeywordSearchGServerAPI/src/main/java/edu/pitt/sis/infsci2711/multidbskeywordsearchgserverapi/server/ServerStart.java
+++ MultiDBsKeywordSearchGServerAPI/src/main/java/edu/pitt/sis/infsci2711/multidbskeywordsearchgserverapi/server/ServerStart.java
@@ -7,9 +7,16 @@ import edu.pitt.sis.infsci2711.multidbskeywordsearchgserverapi.server.JerseyJett
 public class ServerStart {
 
 	public static void main(final String[] args) throws Exception {
-		JerseyJettyServer server = new JerseyJettyServer(7648, "edu.pitt.sis.infsci2711.multidbskeywordsearchgserverapi.rest");
+		JerseyJettyServer server = new JerseyJettyServer(7654, "edu.pitt.sis.infsci2711.multidbskeywordsearchgserverapi.rest");
 		server.start();
+		/*	final Driver driver = new Driver();
+	    final String hostPort = "graph.sb02.stations.graphenedb.com:24789";
 
+	    final Properties neos = new Properties();
+	    neos.put("user", "graph");
+	    neos.put("password", "QoFmuLo7X3alBcCI0bMh");
+	    Neo4jConnection conn = driver.connect("jdbc:neo4j://" + hostPort, neos);
+	    conn.*/
 	}
 	
 }
