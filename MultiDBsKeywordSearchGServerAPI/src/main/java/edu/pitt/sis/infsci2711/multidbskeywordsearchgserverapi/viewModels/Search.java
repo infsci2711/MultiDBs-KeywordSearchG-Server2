025f00dcb249c73932c4a132cba86f1100fe04e9
keranzhao <kez17@pitt.edu>
2015-04-13 20:25:01 -0400

    A7

--- MultiDBsKeywordSearchGServerAPI/src/main/java/edu/pitt/sis/infsci2711/multidbskeywordsearchgserverapi/viewModels/Search.java
+++ MultiDBsKeywordSearchGServerAPI/src/main/java/edu/pitt/sis/infsci2711/multidbskeywordsearchgserverapi/viewModels/Search.java
@@ -66,7 +66,7 @@ public class Search {
 	
 	 public Search () {
 
-		 DB_PATH = "graph.DB";
+		 DB_PATH = "target/graph.DB";
 		 //DB_PATH = "/opt/project/MultiDBs-KeywordSearchG-Server2/MultiDBsKeywordSearchGServerAPI/";
 		 db = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
 
