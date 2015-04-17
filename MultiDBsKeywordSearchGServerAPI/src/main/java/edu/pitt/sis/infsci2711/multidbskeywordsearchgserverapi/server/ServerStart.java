8be2f469edd5ec5b5d959ebeac1ee604af3ff208
keranzhao <kez17@pitt.edu>
2015-04-17 14:56:02 -0400

    Add config.properties and generate test db

--- MultiDBsKeywordSearchGServerAPI/src/main/java/edu/pitt/sis/infsci2711/multidbskeywordsearchgserverapi/server/ServerStart.java
+++ MultiDBsKeywordSearchGServerAPI/src/main/java/edu/pitt/sis/infsci2711/multidbskeywordsearchgserverapi/server/ServerStart.java
@@ -2,21 +2,45 @@ package edu.pitt.sis.infsci2711.multidbskeywordsearchgserverapi.server;
 
 
 
+import java.io.File;
+
 import edu.pitt.sis.infsci2711.multidbskeywordsearchgserverapi.server.JerseyJettyServer;
 
 public class ServerStart {
-
+	private final static String PROPERTY_PORT = "port";
+	private final static int DEFAULT_PORT = 7654;
+	
 	public static void main(final String[] args) throws Exception {
-		JerseyJettyServer server = new JerseyJettyServer(7654, "edu.pitt.sis.infsci2711.multidbskeywordsearchgserverapi.rest");
+		
+		if (args.length > 0) {
+			String propertiesFilePath = args[0];
+			File propertiesFile = new File(propertiesFilePath);
+			PropertiesManager.getInstance().loadProperties(propertiesFile);
+		}
+		
+		final JerseyJettyServer server = new JerseyJettyServer(PropertiesManager.getInstance().getIntProperty(PROPERTY_PORT, DEFAULT_PORT), 
+				"edu.pitt.sis.infsci2711.multidbskeywordsearchgserverapi.rest");
+		
 		server.start();
-		/*	final Driver driver = new Driver();
-	    final String hostPort = "graph.sb02.stations.graphenedb.com:24789";
-
-	    final Properties neos = new Properties();
-	    neos.put("user", "graph");
-	    neos.put("password", "QoFmuLo7X3alBcCI0bMh");
-	    Neo4jConnection conn = driver.connect("jdbc:neo4j://" + hostPort, neos);
-	    conn.*/
+		
+//		Thread serverTread = new Thread(new Runnable() {
+//			
+//			@Override
+//			public void run() {
+//				try {
+//					server.start();
+//				} catch (Exception e) {
+//					throw new RuntimeException(e);
+//				}
+//			}
+//		});
+//		
+//		serverTread.start();
+//		
+//		System.out.println("NOTE: To stop the server, focus on console and hit enter");
+//		System.in.read();
+//		
+//		server.stop();
 	}
 	
 }
